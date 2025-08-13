54a31adceb299e72adcc1a4ba4358aeffd9c492d
khamzaev <khamzaew.j@gmail.com>
2025-08-13 13:21:45 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by khamzaev on 13.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
