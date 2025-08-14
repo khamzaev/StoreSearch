e1428150f57f737df38bc3fae417cd246ca8f095
khamzaev <khamzaew.j@gmail.com>
2025-08-14 13:44:54 +0300

    Bug fix

---
+++ StoreSearch/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by khamzaev on 13.08.2025.
+//
+
+import Foundation
+
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
