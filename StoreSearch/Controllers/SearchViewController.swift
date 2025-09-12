815b5f781d22e6de29289b11af3c6897c16bf3ef
khamzaev <khamzaew.j@gmail.com>
2025-09-12 15:01:23 +0300

    Add Detail Pop-up

--- StoreSearch/Controllers/SearchViewController.swift
+++ StoreSearch/Controllers/SearchViewController.swift
@@ -79,6 +79,18 @@ class SearchViewController: UIViewController {
         alert.addAction(action)
         present(alert,animated: true, completion: nil)
     }
+    
+    
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let detailViewController = segue.destination as! DetailViewController
+            let indexPath = sender as! IndexPath
+            let searchResult = searchResults[indexPath.row]
+            detailViewController.searchResult = searchResult
+        }
+    }
 
 }
 
@@ -172,6 +184,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "ShowDetail", sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
